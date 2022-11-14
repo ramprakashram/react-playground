0e00ed51cccb3f06dade1715e1080fdf6c0dad2e
Akshay Khare <akshay@gravityilabs.com>
2022-11-14 17:27:50 +0530

    Minor changes for connecting to socket

--- src/pages/MapboxDemo/Mapbox.jsx
+++ src/pages/MapboxDemo/Mapbox.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useCallback } from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useRef } from "react";
 import Map from "react-map-gl";
 import MapNavigator from "./MapNavigator";
 import MapPins from "./MapPins";
+import socketIOClient from "socket.io-client";
 
 const ACCESS_TOKEN =
   "pk.eyJ1IjoicmFtcHJha2FzaHJhbSIsImEiOiJjbGE5YjZ4NDAwN2pqM3ZsZGl6cTc4YXpuIn0.bOcJL5grdm7IHzm2eP_xfA";
@@ -17,10 +18,29 @@ const initialViewState = {
   pitch: 0, // tilt of the map - range is 0 to 85
 };
 
+const ENDPOINT = "http://localhost:2000/";
+
+const socket = socketIOClient(ENDPOINT);
+  socket.on("connect", () => {
+    socket.emit("source", "browser");
+  });
 export default function Mapbox() {
   const mapRef = useRef();
   const [currentLocation, setCurrentLocation] = useState();
-
+  useEffect(() => {
+       
+        socket.off("locateMap").on("locateMap", props => {
+          const { latitude, longitude, zoom } = props;
+          mapRef.current?.flyTo({
+            center: [longitude, latitude],
+            duration: 2000,
+            zoom,
+            pitch: 50
+          });
+          setCurrentLocation(props);
+        });
+        
+      }, []);
   const onClickHandle = useCallback((props) => {
     const { latitude, longitude, zoom } = props;
     mapRef.current?.flyTo({
